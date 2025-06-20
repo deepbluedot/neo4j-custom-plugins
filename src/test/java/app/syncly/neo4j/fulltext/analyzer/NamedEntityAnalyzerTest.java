3ff3cde3e2b2dddef0daa00fce6d0dcf9ab82f7c
Kyle Kim <kyle@deepbluedot.io>
2025-06-20 21:28:00 +0900

    Add a simple test for NamedEntityAnalyzer

---
+++ src/test/java/app/syncly/neo4j/fulltext/analyzer/NamedEntityAnalyzerTest.java
@@ -0,0 +1,42 @@
+package app.syncly.neo4j.fulltext.analyzer;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+import org.apache.lucene.analysis.TokenStream;
+import org.junit.Test;
+
+import static org.apache.lucene.analysis.en.EnglishAnalyzer.ENGLISH_STOP_WORDS_SET;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class NamedEntityAnalyzerTest {
+    @Test
+    public void testAnalyzerOutput() throws IOException {
+        String[] inputs = { "Hello World!", "A.B#C", "TeSt\t123" };
+        Analyzer analyzer = new NamedEntityAnalyzer(ENGLISH_STOP_WORDS_SET);
+        List<String> tokenList = new ArrayList<>();
+        for (String input : inputs) {
+            List<String> tokens = analyzeText(analyzer, input);
+            tokenList.add(tokens.get(0));
+            System.out.println("Input: " + input);
+            System.out.println("Tokens: " + tokens);
+        }
+        assertArrayEquals(tokenList.toArray(), new String[] { "helloworld", "abc", "test123" });
+    }
+
+    private List<String> analyzeText(Analyzer analyzer, String text) throws IOException {
+        List<String> result = new ArrayList<>();
+        try (TokenStream tokenStream = analyzer.tokenStream("field", text)) {
+            tokenStream.reset();
+            CharTermAttribute attr = tokenStream.addAttribute(CharTermAttribute.class);
+            while (tokenStream.incrementToken()) {
+                result.add(attr.toString());
+            }
+            tokenStream.end();
+        }
+        return result;
+    }
+}
