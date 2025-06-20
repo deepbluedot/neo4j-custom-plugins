dc5fd64b4f2fe1b2f2c1ce069ad2336c26275000
Kyle Kim <kyle@deepbluedot.io>
2025-06-20 15:40:33 +0900

    Add NamedEntityAnalyzerProvider - but actual analyzer logic has not been implemented yet

---
+++ src/test/java/app/syncly/neo4j/fulltext/analyzer/providers/NamedEntityAnalyzerProviderTest.java
@@ -0,0 +1,24 @@
+package app.syncly.neo4j.fulltext.analyzer.providers;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.Test;
+import org.neo4j.driver.GraphDatabase;
+import org.neo4j.driver.Value;
+import org.neo4j.harness.Neo4j;
+
+import java.io.IOException;
+
+public class NamedEntityAnalyzerProviderTest {
+    private Neo4j embeddedDatabaseServer;
+
+    @BeforeAll
+    void initializeNeo4j() throws IOException {
+        // TODO(Kyle): Implement
+    }
+
+    @Test
+    public void testAnalyzer() {
+        // TODO(Kyle): Implement
+    }
+}
